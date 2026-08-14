59fb2c0f55bf885dbec2404177cf8fbf53532ac4
Itesmarena2 <info@itesmarena.edu.do>
2026-08-14 10:46:18 -0400

    restaurado

--- js/openlinknew.js
+++ js/openlinknew.js
@@ -2,7 +2,6 @@ document.addEventListener("DOMContentLoaded", function () {
     var links = document.getElementsByTagName('a');
     for (var i = 0; i < links.length; i++) {
         if (links[i].hostname != window.location.hostname) {
-            console.log(links[i]);
             if(["Ir a Guía Conservación de suelo", "Ir a Historia"].includes(links[i].title)) {
                 links[i].target = '_blank';
                 links[i].rel = 'noopener noreferrer';
