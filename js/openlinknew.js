c829fd6c58263d33f8ee9c97f250e3e892d3d9c4
Itesmarena2 <info@itesmarena.edu.do>
2026-08-14 10:33:10 -0400

    probando

--- js/openlinknew.js
+++ js/openlinknew.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
     var links = document.links;
     for (var i = 0; i < links.length; i++) {
         if (links[i].hostname != window.location.hostname) {
-            if(links[i].title == "Ir a Guía Conservación de suelo"){
+            if(["Ir a Guía Conservación de suelo", "Ir a Historia"].includes(links[i].title)) {
                 links[i].target = '_blank';
                 links[i].rel = 'noopener noreferrer';
                 
