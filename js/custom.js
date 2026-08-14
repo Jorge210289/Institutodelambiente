cd8b24c5ac8e3fd03b80f5d2a2ba0b036ebc89bf
Itesmarena2 <info@itesmarena.edu.do>
2026-08-14 10:12:23 -0400

    Justes

---
+++ js/custom.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var links = document.links;
+    for (var i = 0; i < links.length; i++) {
+        if (links[i].hostname != window.location.hostname) {
+            console.log(links[i]);
+            links[i].target = '_blank';
+            links[i].rel = 'noopener noreferrer';
+        }
+    }
+});
