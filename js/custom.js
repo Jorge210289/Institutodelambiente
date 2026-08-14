a4cdfea70eb92104c11d77dc71301f2c5a55c3a0
Itesmarena2 <info@itesmarena.edu.do>
2026-08-14 10:20:58 -0400

    Limpiando script

--- js/custom.js
+++ js/custom.js
@@ -2,7 +2,6 @@ document.addEventListener("DOMContentLoaded", function () {
     var links = document.links;
     for (var i = 0; i < links.length; i++) {
         if (links[i].hostname != window.location.hostname) {
-            console.log(links[i].title);
             if(links[i].title == "Ir a Guía Conservación de suelo"){
                 links[i].target = '_blank';
                 links[i].rel = 'noopener noreferrer';
