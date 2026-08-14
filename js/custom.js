5446d79c63b7204324437a2d3a9f6b4935eec9b3
Itesmarena2 <info@itesmarena.edu.do>
2026-08-14 10:17:32 -0400

    Title

--- js/custom.js
+++ js/custom.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
     var links = document.links;
     for (var i = 0; i < links.length; i++) {
         if (links[i].hostname != window.location.hostname) {
-            console.log(links[i]);
+            console.log(links[i].attr('title'));
             if(links.title == "Ir a Guía Conservación de suelo"){
                 links[i].target = '_blank';
                 links[i].rel = 'noopener noreferrer';
